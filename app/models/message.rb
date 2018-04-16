d299a15360df42f01a8fbb30182ab124e4c555db
Samantha Rabling <sjarillorabling@gmail.com>
2018-04-15 18:20:59 -0700

    fixed bugs

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   has_many :comments
-  validates presence: true, length: {minimum: 20}
+  validates :message, presence: true, length: {minimum: 20}
 end
